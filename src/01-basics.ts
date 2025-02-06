7e1cd6a6608536facc6d5c2e47988c7bd3d8c51c
Wu SongYu <wuwilliam2002@gmail.com>
2025-02-06 13:24:53 +0000

    Demo optional properties and parameters.

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -1,15 +1,17 @@
 import {Friend, Colleague, ColleagueHistory } from './myTypes'
 
-const friend1 = {
+const friend1 : Friend = {
     name: "Paul Fleming",
     phone: "087-12345",
     age: 25,
+    dob: new Date("1998-11-20") ,
   };
   
-  const friend2 = {
+  const friend2 : Friend = {
     name: "Jane Costello",
     phone: "086--12345",
     age: 31,
+    interests: ['Music', 'Sport']
   };
 
   
